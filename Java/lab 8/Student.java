80460e8904f08bc5e7cdcc9c3e4087d2e5ab8401
TH2302023 DT <dtranjack@gmail.com>
2023-10-25 23:15:33 +0700

    Added a menu, added a lot of methods into Student class

--- Java/lab 8/Student.java
+++ Java/lab 8/Student.java
@@ -1,18 +1,26 @@
-package session7.entity;
+package lab8;
 
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
 
 public class Student {
-    private int id;
+    private final int id;
     private String name;
     private String address;
     private String phoneNumber;
+    private Date DoB;
+    private Date entryDate;
 
-    public Student(int id, String name, String address, String phoneNumber) {
+    public Student(int id, String name, String address, String phoneNumber, Date DoB, Date entryDate) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
+        this.DoB = DoB;
+        this.entryDate = entryDate;
     }
 
     public static Student inputInfo() {
@@ -27,20 +35,230 @@ public class Student {
         String address = scanner.nextLine();
         System.out.print("Enter 7-Digit Phone Number: ");
         String phoneNumber = scanner.next();
+        scanner.nextLine();
+        System.out.print("Enter Date of Birth in the (yy-MM-dd) format: ");
+        String DoBstr = scanner.nextLine();
+        System.out.print("Enter Date of enrollment in the (yy-MM-dd) format: ");
+        String entryDatestr = scanner.nextLine();
 
-        return new Student(id, name, address, phoneNumber);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dob = null;
+        Date entryDate = null;
+
+        try {
+            dob = dateFormat.parse(DoBstr);
+            entryDate = dateFormat.parse(entryDatestr);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return new Student(id, name, address, phoneNumber, dob, entryDate);
     }
-    
+
     public int getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public String getAddress() {
         return address;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public Integer getAge() {
+        if (this.DoB != null) {
+            // Calculate age based on the DoB
+            Instant dobDate = this.DoB.toInstant();
+            Instant currentDate = Instant.now();
+            Duration duration = Duration.between(dobDate, currentDate);
+            long years = duration.toDays() / 365;
+            return Math.toIntExact(years);
+        }
+        return null;
+    }
+
+    public Integer getLevel() {
+        if (this.entryDate != null) {
+            Instant entryDate = this.entryDate.toInstant();
+            Instant currentDate = Instant.now();
+            Duration duration = Duration.between(entryDate, currentDate);
+            long years = duration.toDays() / 365;
+            return Math.toIntExact(years);
+        }
+        return null;
+    }
+
+    private static final List<Student> studentList = new ArrayList<>();
+
+    public static Student addStudent() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            Student student = Student.inputInfo();
+            studentList.add(student);
+
+            System.out.print("Do you want to add another student (y/n)? ");
+            String another = scanner.next();
+            if (!another.equalsIgnoreCase("y")) {
+                break;
+            }
+        }
+
+        studentList.sort(Comparator.comparingInt(Student::getId));
+        return studentList.get(studentList.size() - 1);
+    }
+
+    public static List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public static void showStudent() {
+        System.out.println("Student List (Sorted by ID in Ascending Order):");
+        for (Student s : studentList) {
+            System.out.println("ID: " + s.getId());
+            System.out.println("Name: " + s.getName());
+            System.out.println("Address: " + s.getAddress());
+            System.out.println("Phone Number: " + s.getPhoneNumber());
+            System.out.println("Age: " + s.getAge());
+            System.out.println("Level: " + s.getLevel() + "\n");
+
+        }
+    }
+
+    public static void updateStudent(List<Student> studentList, int studentId, Scanner scanner) {
+        // Find the student with the specified ID
+        Student studentToEdit = null;
+        for (Student student : studentList) {
+            if (student.getId() == studentId) {
+                studentToEdit = student;
+                break;
+            }
+        }
+
+        if (studentToEdit == null) {
+            System.out.println("Student with ID " + studentId + " not found.");
+        } else {
+            // Display the current student information
+            System.out.println("Current Information for Student ID " + studentToEdit.getId() + ":");
+            System.out.println("Name: " + studentToEdit.getName());
+            System.out.println("Address: " + studentToEdit.getAddress());
+            System.out.println("Phone Number: " + studentToEdit.getPhoneNumber());
+            // Add more student details as needed
+
+            // Now implement the logic to edit the student's information
+            System.out.print("Enter the new name (press Enter to keep the current value): ");
+            String newName = scanner.nextLine();
+            if (!newName.isEmpty()) {
+                studentToEdit.name = newName;
+            }
+
+            System.out.print("Enter the new address (press Enter to keep the current value): ");
+            String newAddress = scanner.nextLine();
+            if (!newAddress.isEmpty()) {
+                studentToEdit.address = newAddress;
+            }
+
+            System.out.print("Enter the new phone number (press Enter to keep the current value): ");
+            String newPhoneNumber = scanner.nextLine();
+            if (!newPhoneNumber.isEmpty()) {
+                studentToEdit.phoneNumber = newPhoneNumber;
+            }
+
+            System.out.print("Enter the new Date of Birth in the (yy-MM-dd) format (press Enter to keep the current value): ");
+            String newDoB = scanner.nextLine();
+            if (!newDoB.isEmpty()) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd");
+                try {
+                    Date newDoBDate = dateFormat.parse(newDoB);
+                    studentToEdit.DoB = newDoBDate;
+                } catch (ParseException e) {
+                    System.out.println("Invalid date format. Date of Birth not updated.");
+                }
+            }
+
+            System.out.print("Enter the new entry date (yy-MM-dd format) (press Enter to keep the current value): ");
+            String newEntryDate = scanner.nextLine();
+            if (!newEntryDate.isEmpty()) {
+                SimpleDateFormat entryDateFormat = new SimpleDateFormat("yy-MM-dd");
+                try {
+                    studentToEdit.entryDate = entryDateFormat.parse(newEntryDate);
+                } catch (ParseException e) {
+                    System.out.println("Invalid date format. Entry date not updated.");
+                }
+            }
+
+            System.out.println("Student information updated.");
+        }
+    }
+
+    public static void removeStudent(List<Student> studentList, int studentId, Scanner scanner) {
+        Student studentToRemove = null;
+        for (Student student : studentList) {
+            if (student.getId() == studentId) {
+                studentToRemove = student;
+                break;
+            }
+        }
+        if (studentToRemove == null) {
+            System.out.println("Student with ID " + studentId + " not found.");
+        } else {
+            studentList.remove(studentToRemove);
+            System.out.println("Student removed.");
+        }
+    }
+
+    public static void showStudentWithSameAge(List<Student> studentList, int filterAge) {
+        List<Student> studentsWithSameAge = new ArrayList<>();
+        for (Student student : studentList) {
+            Integer age = student.getAge();
+            if (age != null && age == filterAge) {
+                studentsWithSameAge.add(student);
+            }
+        }
+
+        if (studentsWithSameAge.isEmpty()) {
+            System.out.println("No students with the specified age found.");
+        } else {
+            System.out.println("Students with age " + filterAge + ":");
+            for (Student s : studentsWithSameAge) {
+                System.out.println("ID: " + s.getId());
+                System.out.println("Name: " + s.getName());
+                System.out.println("Address: " + s.getAddress());
+                System.out.println("Phone Number: " + s.getPhoneNumber());
+                System.out.println("Age: " + s.getAge());
+                System.out.println("Level: " + s.getLevel() + "\n");
+            }
+        }
+    }
+
+    public static void showStudentWithLevel(List<Student> studentList, int filterLevel) {
+        List<Student> studentsWithSameLevel = new ArrayList<>();
+        for (Student student : studentList) {
+            Integer level = student.getLevel();
+            if (level != null && level == filterLevel) {
+                studentsWithSameLevel.add(student);
+            }
+        }
+
+        if (studentsWithSameLevel.isEmpty()) {
+            System.out.println("No students with the specified level found.");
+        } else {
+            System.out.println("Students from the level " + filterLevel + ":");
+            for (Student s : studentsWithSameLevel) {
+                System.out.println("ID: " + s.getId());
+                System.out.println("Name: " + s.getName());
+                System.out.println("Address: " + s.getAddress());
+                System.out.println("Phone Number: " + s.getPhoneNumber());
+                System.out.println("Age: " + s.getAge());
+                System.out.println("Level: " + s.getLevel() + "\n");
+            }
+        }
     }
 }
--- Java/lab 8/StudentApp.java
+++ Java/lab 8/StudentApp.java
@@ -1,32 +1,111 @@
-package session7.entity;
+package lab8;
 
 import java.util.*;
 
 public class StudentApp {
     public static void main(String[] args) {
-        List<Student> studentList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            Student student = Student.inputInfo();
-            studentList.add(student);
+            // Display the menu
+            System.out.println("Menu:");
+            System.out.println("1. Add More Student");
+            System.out.println("2. Show Student List");
+            System.out.println("3. Add More Students");
+            System.out.println("4. Remove a Student");
+            System.out.println("5. Show Students with the Same Age");
+            System.out.println("6. Show Students with the Same Level");
+            System.out.println("7. Save Student List to a Text File");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
 
-            System.out.print("Do you want to add another student (y/n)? ");
-            String another = scanner.next();
-            if (!another.toLowerCase().equals("y")) {
-                break;
+            // Read the user's choice
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline
+
+            switch (choice) {
+                case 1:
+                    // Add a student
+                    Student newStudent = Student.addStudent();
+                    System.out.println("Student added: ID - " + newStudent.getId() + ", Name - " + newStudent.getName());
+                    break;
+
+                case 2:
+                    // Show the student list
+                    List<Student> studentList = Student.getStudentList();
+                    if (studentList.isEmpty()) {
+                        System.out.println("No students existed.");
+                    } else {
+                        Student.showStudent();
+                        }
+                    break;
+
+                case 3:
+                    //edit a student using ID
+                    studentList = Student.getStudentList();
+                    if (studentList.isEmpty()) {
+                        System.out.println("No students existed.");
+                    } else {
+                        System.out.print("Enter the ID of the student to edit: ");
+                        int editStudentId = scanner.nextInt();
+                        scanner.nextLine();
+                        Student.updateStudent(studentList, editStudentId, scanner);
+                    }
+                    break;
+
+                case 4:
+                    // Remove a student using ID
+                    studentList = Student.getStudentList();
+                    if (studentList.isEmpty()) {
+                        System.out.println("No students existed.");
+                    } else {
+                        System.out.print("Enter the ID of the student to remove: ");
+                        int removeStudentId = scanner.nextInt();
+                        scanner.nextLine();
+                        Student.removeStudent(studentList, removeStudentId, scanner);
+                    }
+                    break;
+
+                case 5:
+                    // Show students with the same age
+                    studentList = Student.getStudentList();
+                    if (studentList.isEmpty()) {
+                        System.out.println("No students existed.");
+                    } else {
+                        System.out.print("Enter the age to filter the student of that age: ");
+                        int filterAge = scanner.nextInt();
+                        scanner.nextLine();
+                        Student.showStudentWithSameAge(studentList, filterAge);
+                    }
+                    break;
+
+                case 6:
+                    // Show students with the same level
+                    studentList = Student.getStudentList();
+                    if (studentList.isEmpty()) {
+                        System.out.println("No students existed.");
+                    } else {
+                        System.out.print("Enter the level to filter the student of that level: ");
+                        int filterLevel = scanner.nextInt();
+                        scanner.nextLine();
+                        Student.showStudentWithLevel(studentList, filterLevel);
+                    }
+                    break;
+
+                case 7:
+                    // Todo: Save student list to a text file and exit the program (use singleton)
+                    // ...
+                    break;
+
+                case 8:
+                    // Exit the application
+                    System.out.println("Exiting the application.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+                    break;
             }
-        }
-
-        studentList.sort(Comparator.comparingInt(Student::getId));
-
-        System.out.println("Student List (Sorted by ID in Ascending Order):");
-        for (Student s : studentList) {
-            System.out.println("ID: " + s.getId());
-            System.out.println("Name: " + s.getName());
-            System.out.println("Address: " + s.getAddress());
-            System.out.println("Phone Number: " + s.getPhoneNumber());
-            System.out.println();
         }
     }
 }
